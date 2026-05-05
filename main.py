374889ce518b46f48a6de6507bef2a6d8a78cbb5
superma30 <126937712+superma30@users.noreply.github.com>
2026-05-05 17:44:52 +0200

    Built more framework

--- main.py
+++ main.py
@@ -1,35 +1,83 @@
 import cv2
+import scripts.filters
+import scripts.effects
 from scripts.ui import UI
 
-cap = cv2.VideoCapture(0)
-ui = UI()
-running = True
+class StateHandler:
+    def __init__(self, filters, effects):
+        self.filters = filters
+        self.effects = effects
+        self.currFilter = 0
+        self.currEffect = 0
+    def scroll(self, name, q):
+        if name == "filters":
+            n = len(self.filters)
+            if n == 0:
+                return
+            self.currFilter = (self.currFilter + q) % n
+            return filters[self.currFilter]
+        elif name == "effects":
+            n = len(self.effects)
+            if n == 0:
+                return
+            self.currEffect = (self.currEffect + q) % n
+            return effects[self.currEffect]
 
-while running:
+
+def applyFilter(filters, filter, frame):
+    return frame
+def applyEffect(effects, effect, frame):
+    return frame
+
+
+if __name__ == "__main__":
+    filters = (None, "idk", "idk2")
+    effects = (None, "i", "ii", "iii", "iiii")
+
+    cap = cv2.VideoCapture(0)
+    ui = UI()
+    state = StateHandler(filters, effects)
+    running = True
+
     try:
-        ret, frame = cap.read()
-        if not ret:
-            print("Error while reading from webcam") # Make this more stable, maybe show a visual hint and then close the app after a few errors or wait until it works
-            continue
+        filterName = None
+        effectName = None
+        while running:
+            ret, frame = cap.read()
+            if not ret:
+                print(
+                    "Error while reading from webcam")  # Make this more stable, maybe show a visual hint and then close the app after a few errors or wait until it works
+                continue
 
-        # input
-        key = cv2.waitKey(1) & 0xFF
+            # input
+            key = cv2.waitKey(1) & 0xFF
 
-        if key == ord("q") or key == ord("Q") or key == 27:
-            running = False
+            if key == ord("q") or key == ord("Q") or key == 27:
+                running = False
 
-        elif key == ord(""): # Add other keys and structure
-            pass
+            elif key == ord("w"):
+                filterName = state.scroll("filters",1)
+            elif key == ord("s"):
+                filterName = state.scroll("filters",-1)
 
-        # UI and HUD
-        frame = ui.draw(
-            frame,
-            filter_name="",
-            face_count="",
-            fps=cap.get(cv2.CAP_PROP_FPS)
-        )
+            elif key == ord("a"):
+                effectName = state.scroll("effects",-1)
+            elif key == ord("d"):
+                effectName = state.scroll("effects",1)
 
-        cv2.imshow("Webcam", frame)
+            frame = applyFilter(filters, filterName, frame)
+            frame = applyEffect(effects, effectName, frame)
+
+            # UI and HUD
+            frame = ui.draw(
+                frame,
+                filter_name=filterName,
+                effect_name=effectName,
+                face_count="",
+                fps=cap.get(cv2.CAP_PROP_FPS)
+            )
+
+            cv2.imshow("Webcam", frame)
     finally:
         cap.release()
         cv2.destroyAllWindows()
--- scripts/ui.py
+++ scripts/ui.py
@@ -6,19 +6,23 @@ class UI:
     def __init__(self):
         pass
 
-    def draw(self, frame, filter_name = None, face_count = None, fps = None):
+    def draw(self, frame, filter_name = None, effect_name = None, face_count = None, fps = None):
         frame = frame.copy()
 
         if filter_name is not None:
-            cv2.putText(frame, f"Filter: {filter_name}", (10, 30),
+            cv2.putText(frame, f"Filter: {filter_name}", (10, np.shape(frame)[0] - 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 2)
+
+        if effect_name is not None:
+            cv2.putText(frame, f"Effect: {effect_name}", (np.shape(frame)[1] - (len(effect_name) * 7 + 90), np.shape(frame)[0] - 30),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 2)
 
         if face_count is not None:
-            cv2.putText(frame, f"Faces: {face_count}", (10, 60),
+            cv2.putText(frame, f"Faces: {face_count}", (10, 30),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 2)
 
         if fps is not None:
-            cv2.putText(frame, f"FPS: {int(fps)}", (10, 90),
+            cv2.putText(frame, f"FPS: {int(fps)}", (10, 60),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 2)
 
         return frame
